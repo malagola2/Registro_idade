ef62d24bd23503df2168f066ae0ad9aa65b21495
RAFAEL SARAGOCA MALAGOLA <rafael.smalagola@senacsp.edu.br>
2024-09-24 15:23:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+    let nome = prompt ("insira o seu nome")
+    
+    let idade = parseInt (prompt("idade do usuário"))
+
+//calcular 
+let anosparacem = 100 - idade
+
+//Apresentar para o usuário um alerta
+alert(`${nome},faltam ${anosparacem} anos para você completar 100 anos`);
